4bd6b9f53a3081032280a669e35189032092610d
phlukas <lukas.veckys@gmail.com>
2021-03-11 17:02:13 +0200

    Initial create

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OfficeOpenXml;
+
+namespace MitybosPlanas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            InitializeComponent();
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            CreateExcel("Testas");
+        }
+
+        private void CreateExcel(string fileName)
+        {
+            string savePath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+
+            
+        }
+    }
+}
