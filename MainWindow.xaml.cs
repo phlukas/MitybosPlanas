e8838ce0cc240fd3ded98d9abf602f6d87d9259b
phlukas <lukas.veckys@gmail.com>
2021-03-19 14:24:21 +0200

    display recipes below

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Windows;
 using OfficeOpenXml;
+using OfficeOpenXml.Style;
 
 namespace MitybosPlanas
 {
@@ -99,7 +101,8 @@ namespace MitybosPlanas
             FileInfo logo = new FileInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logo.PNG"));
             OfficeOpenXml.Drawing.ExcelPicture pic = sheet.Drawings.AddPicture("logo", logo);
             sheet.SelectedRange[1, 1, 1, 5].Merge = true;
-            sheet.Row(1).Height = 140;
+            sheet.Row(1).Height = 55;
+            pic.SetSize(50);
             pic.SetPosition(0, 0);
         }
 
@@ -107,75 +110,127 @@ namespace MitybosPlanas
         {
             try
             {
-                Recipe recipe = (Recipe)comboBox.SelectedItem;
+                List<Recipe> selectedRecipes = new List<Recipe>(12);
 
                 sheet.Cells[3, 1].Value = "Pusryčiai";
                 sheet.Cells[5, 1].Value = "Pietūs";
                 sheet.Cells[7, 1].Value = "Vakarienė";
 
                 //Pusryčiai
+                Recipe recipe = (Recipe)comboBox.SelectedItem;
+                AddRecipeIfUnique(selectedRecipes, recipe);
+
                 sheet.Cells[3, 2].Value = recipe.Title;
                 sheet.Cells[4, 2].Value = recipe.Ingredients;
 
                 recipe = (Recipe)comboBox1.SelectedItem;
+                AddRecipeIfUnique(selectedRecipes, recipe);
 
                 sheet.Cells[3, 3].Value = recipe.Title;
                 sheet.Cells[4, 3].Value = recipe.Ingredients;
 
                 recipe = (Recipe)comboBox2.SelectedItem;
+                AddRecipeIfUnique(selectedRecipes, recipe);
 
                 sheet.Cells[3, 4].Value = recipe.Title;
                 sheet.Cells[4, 4].Value = recipe.Ingredients;
 
                 recipe = (Recipe)comboBox3.SelectedItem;
+                AddRecipeIfUnique(selectedRecipes, recipe);
 
                 sheet.Cells[3, 5].Value = recipe.Title;
                 sheet.Cells[4, 5].Value = recipe.Ingredients;
 
                 //Pietūs
                 recipe = (Recipe)comboBox_Copy.SelectedItem;
+                AddRecipeIfUnique(selectedRecipes, recipe);
+
                 sheet.Cells[5, 2].Value = recipe.Title;
                 sheet.Cells[6, 2].Value = recipe.Ingredients;
 
                 recipe = (Recipe)comboBox1_Copy.SelectedItem;
+                AddRecipeIfUnique(selectedRecipes, recipe);
 
                 sheet.Cells[5, 3].Value = recipe.Title;
                 sheet.Cells[6, 3].Value = recipe.Ingredients;
 
                 recipe = (Recipe)comboBox2_Copy.SelectedItem;
+                AddRecipeIfUnique(selectedRecipes, recipe);
 
                 sheet.Cells[5, 4].Value = recipe.Title;
                 sheet.Cells[6, 4].Value = recipe.Ingredients;
 
                 recipe = (Recipe)comboBox3_Copy.SelectedItem;
+                AddRecipeIfUnique(selectedRecipes, recipe);
 
                 sheet.Cells[5, 5].Value = recipe.Title;
                 sheet.Cells[6, 5].Value = recipe.Ingredients;
 
                 //Vakarienė
                 recipe = (Recipe)comboBox_Copy1.SelectedItem;
+                AddRecipeIfUnique(selectedRecipes, recipe);
+
                 sheet.Cells[7, 2].Value = recipe.Title;
                 sheet.Cells[8, 2].Value = recipe.Ingredients;
 
                 recipe = (Recipe)comboBox1_Copy1.SelectedItem;
+                AddRecipeIfUnique(selectedRecipes, recipe);
 
                 sheet.Cells[7, 3].Value = recipe.Title;
                 sheet.Cells[8, 3].Value = recipe.Ingredients;
 
                 recipe = (Recipe)comboBox2_Copy1.SelectedItem;
+                AddRecipeIfUnique(selectedRecipes, recipe);
 
                 sheet.Cells[7, 4].Value = recipe.Title;
                 sheet.Cells[8, 4].Value = recipe.Ingredients;
 
                 recipe = (Recipe)comboBox3_Copy1.SelectedItem;
+                AddRecipeIfUnique(selectedRecipes, recipe);
 
                 sheet.Cells[7, 5].Value = recipe.Title;
                 sheet.Cells[8, 5].Value = recipe.Ingredients;
+
+                DisplayRecipes(selectedRecipes, 9, sheet);
             }
             catch (Exception)
             {
                 throw;
             }
+        }
+
+        private void DisplayRecipes(List<Recipe> selectedRecipes, int row, ExcelWorksheet sheet)
+        {
+            foreach (Recipe recipe in selectedRecipes)
+            {
+                if (recipe.Description.Length > 1)
+                {
+                    sheet.Cells[row, 1, row, 5].Merge = true;
+                    sheet.Cells[row, 1, row, 5].Value = recipe.Title + " - " + recipe.Description;
+                    sheet.Row(row).Height = MeasureTextHeight(sheet.Cells[row, 1, row, 5].Text, sheet.Cells[row, 1, row, 5].Style.Font, 5);
+                    row++;
+                }
+            }
+        }
+
+        private double MeasureTextHeight(string text, ExcelFont font, double width)
+        {
+            var bitmap = new Bitmap(1, 1);
+            var graphics = Graphics.FromImage(bitmap);
+
+            var pixelWidth = Convert.ToInt32(width * 7);  //7 pixels per excel column width
+            var fontSize = font.Size * 1.01f;
+            var drawingFont = new Font(font.Name, fontSize);
+            var size = graphics.MeasureString(text, drawingFont, pixelWidth, new StringFormat { FormatFlags = StringFormatFlags.MeasureTrailingSpaces });
+
+            //72 DPI and 96 points per inch.  Excel height in points with max of 409 per Excel requirements.
+            return Math.Min(Convert.ToDouble(size.Height) * 72 / 96, 409) + 10;
+        }
+
+        private void AddRecipeIfUnique(List<Recipe> list, Recipe recipe)
+        {
+            if (!list.Contains(recipe))
+                list.Add(recipe);
         }
 
         private void FillComboBoxes()
